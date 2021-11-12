ab6b153cb5a40d7618d3d4e5e92ea03dc99a93a5
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-11-12 12:46:32 +0200

    migrations

---
+++ src/Service.BonusClientContext.Postgres/Migrations/20211112104230_DateTimeUtc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.BonusClientContext.Postgres.Migrations
+{
+    public partial class DateTimeUtc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastRecord",
+                schema: "bonusprogram",
+                table: "clientcontexts",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastRecord",
+                schema: "bonusprogram",
+                table: "clientcontexts",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
