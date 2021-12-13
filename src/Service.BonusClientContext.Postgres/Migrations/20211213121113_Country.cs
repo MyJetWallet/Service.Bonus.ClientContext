d085f65c88f5d41391f1e7ee94338c972b9872c7
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-12-13 14:11:32 +0200

    migration fix

---
+++ src/Service.BonusClientContext.Postgres/Migrations/20211213121113_Country.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.BonusClientContext.Postgres.Migrations
+{
+    public partial class Country : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                schema: "bonusprogram",
+                table: "clientcontexts",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                schema: "bonusprogram",
+                table: "clientcontexts");
+        }
+    }
+}
